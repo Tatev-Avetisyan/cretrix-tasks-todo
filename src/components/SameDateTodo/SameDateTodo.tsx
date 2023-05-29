7c2d4dbc43a60c8c8f9e73e4cb2b701edcb9d3e2
Tatevik Avetisyan <karotatev@gmail.com>
2023-05-30 02:15:00 +0400

    todo by date component's bgColor

--- src/components/SameDateTodo/SameDateTodo.tsx
+++ src/components/SameDateTodo/SameDateTodo.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch, useAppSelector } from "../../redux/store";
 import { TodoListType, deleteTodo, todo } from "../../redux/TodoSlice";
 
 import styles from "./sameDateTodo.module.scss";
+import "../Main/main.module.scss";
 
 const SameDateTodoList = () => {
   const dispatch = useAppDispatch();
@@ -25,34 +26,36 @@ const SameDateTodoList = () => {
   console.log(filteredByDate);
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.header}>
-        <button onClick={handleGoBack} className={styles.backBtn}>
-          <img src={BackIcon} alt="Back" />
-          Go Back
-        </button>
-        <Title
-          extraStyle={styles.text}
-          context={`${reverseDate || ""}(${count})`}
-        />
-      </div>
-      <div className={styles.taskList}>
-        {filteredByDate.map((item: TodoListType) => {
-          return (
-            <div
-              key={item.id}
-              style={{ width: "100%", display: item.isHidden ? "none" : "" }}
-            >
-              <SameDateTodoItem
-                item={item}
-                onClick={() => {
-                  dispatch(deleteTodo({ id: item.id }));
-                }}
-                todoName={item.todoName}
-              />
-            </div>
-          );
-        })}
+    <div className="main">
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <button onClick={handleGoBack} className={styles.backBtn}>
+            <img src={BackIcon} alt="Back" />
+            Go Back
+          </button>
+          <Title
+            extraStyle={styles.text}
+            context={`${reverseDate || ""}(${count})`}
+          />
+        </div>
+        <div className={styles.taskList}>
+          {filteredByDate.map((item: TodoListType) => {
+            return (
+              <div
+                key={item.id}
+                style={{ width: "100%", display: item.isHidden ? "none" : "" }}
+              >
+                <SameDateTodoItem
+                  item={item}
+                  onClick={() => {
+                    dispatch(deleteTodo({ id: item.id }));
+                  }}
+                  todoName={item.todoName}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
